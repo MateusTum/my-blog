6782252ed993ff222956def7a01c6d906b017be8
MateusTum <83322634+MateusTum@users.noreply.github.com>
2023-11-16 15:17:46 -0300

    Fix import flask_gravatar

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-# from flask_gravatar import Gravatar
+from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
